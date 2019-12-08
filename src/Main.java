5b16ec12cb386ceca0dc4a0e7ac054554ae3a750
Jonathan <jpmorlen@gmail.com>
2019-12-07 22:56:35 -0500

    Added file reading

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.io.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        // Open file and read line by line
+        try(BufferedReader reader = new BufferedReader(new FileReader(args[0])))
+        {
+            for(String line; (line = reader.readLine()) != null;)
+            {
+                System.out.println(line);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
